ae1f072e3bcb87eac51263354014d3f1fb32ec3f
Yuya Kawamoto <ykawamoto.webdev@gmail.com>
2024-09-07 12:05:56 +0900

    ok

--- app/_section/experience/Experience.tsx
+++ app/_section/experience/Experience.tsx
@@ -8,7 +8,7 @@ import { MdWorkOutline } from "react-icons/md";
 const experience = [
   {
     start: "2023/1",
-    end: "2023/10",
+    end: "2023/9",
     title: "建築業者の案件管理システム",
     des: "大元の建設業者から他業者に案件を振り分けるシステムの開発に携わりました。案件管理、支払請求、発注等の管理が出来るシステムです。私はユーザー管理の機能、請求書発行機能、銀行・会計CSVの発行機構、外部連携API実装など多岐にわたり、かかわることが出来ました。",
     skills: ["Laravel", "Jquery", "MySql"],
